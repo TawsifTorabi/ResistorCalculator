bd4a8917899e549362a9c106a15b764e6e9da7bb
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2016-09-18 15:21:18 +0600

    Add files via upload
    Simulate the image of the resistor

---
+++ simulate.js
@@ -0,0 +1,92 @@
+
+function simulatefour(){
+	var number1 = document.getElementById('one').selectedIndex;
+	var number2 = document.getElementById('two').selectedIndex;
+	var number3 = document.getElementById('four').selectedIndex;
+	var number4 = document.getElementById('five').selectedIndex;
+	
+	var color1 =document.getElementById('one').options[number1].style.backgroundColor;
+	var color2 =document.getElementById('two').options[number2].style.backgroundColor;
+	var color3 =document.getElementById('four').options[number3].style.backgroundColor;
+	var color4 =document.getElementById('five').options[number4].style.backgroundColor;
+
+	  var band1 = document.getElementById('canvas-band1');
+	  var band2 = document.getElementById('canvas-band2');
+	  var band3 = document.getElementById('canvas-band3');
+	  var band4 = document.getElementById('canvas-band4');
+	  
+
+	band1.style.backgroundColor = color1;
+	band2.style.backgroundColor = color2;
+	band3.style.backgroundColor = color3;
+	band4.style.backgroundColor = color4;
+	
+	
+	}
+	
+	
+	
+	function simulatefive(){
+	var number1 = document.getElementById('one').selectedIndex;
+	var number2 = document.getElementById('two').selectedIndex;
+	var number3 = document.getElementById('three').selectedIndex;
+	var number4 = document.getElementById('four').selectedIndex;
+	var number5 = document.getElementById('five').selectedIndex;
+	
+	var color1 =document.getElementById('one').options[number1].style.backgroundColor;
+	var color2 =document.getElementById('two').options[number2].style.backgroundColor;
+	var color3 =document.getElementById('three').options[number3].style.backgroundColor;
+	var color4 =document.getElementById('four').options[number4].style.backgroundColor;
+	var color5 =document.getElementById('five').options[number5].style.backgroundColor;
+
+	  var band1 = document.getElementById('canvas-band1');
+	  var band2 = document.getElementById('canvas-band2');
+	  var band3 = document.getElementById('canvas-band3');
+	  var band4 = document.getElementById('canvas-band4');
+	  var band5 = document.getElementById('canvas-band5');
+	  
+
+	band1.style.backgroundColor = color1;
+	band2.style.backgroundColor = color2;
+	band3.style.backgroundColor = color3;
+	band4.style.backgroundColor = color4;
+	band5.style.backgroundColor = color5;
+	
+	
+	}
+	
+	
+
+	function simulatesix(){
+	var number1 = document.getElementById('one').selectedIndex;
+	var number2 = document.getElementById('two').selectedIndex;
+	var number3 = document.getElementById('three').selectedIndex;
+	var number4 = document.getElementById('four').selectedIndex;
+	var number5 = document.getElementById('five').selectedIndex;
+	var number6 = document.getElementById('six').selectedIndex;
+	
+	var color1 =document.getElementById('one').options[number1].style.backgroundColor;
+	var color2 =document.getElementById('two').options[number2].style.backgroundColor;
+	var color3 =document.getElementById('three').options[number3].style.backgroundColor;
+	var color4 =document.getElementById('four').options[number4].style.backgroundColor;
+	var color5 =document.getElementById('five').options[number5].style.backgroundColor;
+	var color6 =document.getElementById('six').options[number6].style.backgroundColor;
+
+	  var band1 = document.getElementById('canvas-band1');
+	  var band2 = document.getElementById('canvas-band2');
+	  var band3 = document.getElementById('canvas-band3');
+	  var band4 = document.getElementById('canvas-band4');
+	  var band5 = document.getElementById('canvas-band5');
+	  var band6 = document.getElementById('canvas-band6');
+	  
+
+	band1.style.backgroundColor = color1;
+	band2.style.backgroundColor = color2;
+	band3.style.backgroundColor = color3;
+	band4.style.backgroundColor = color4;
+	band5.style.backgroundColor = color5;
+	band6.style.backgroundColor = color6;
+	
+	
+	}
+	
